2d0f37309b7ac2d8343423743115af5754e87e36
Exulenood <sestak-p@outlook.com>
2025-01-13 15:07:16 +0100

    enhance build.zig incl option to build for Windows

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Its just a rectangle on a white surface to be moved with the keyboard's arrow-ke
 To compile:
 
 1. install zig: https://ziglang.org/learn/getting-started/
-2. install SDL2: https://wiki.libsdl.org/SDL2/Installation
+2. install SDL2: https://wiki.libsdl.org/SDL2/Installation (To build on MacOs: if you dont use Homebrew, you have to alter the Path-handling in the build.zig).
 3. run zig build
 
 The executable is generated in <project-folder>/zig-out/bin
--- build.zig
+++ build.zig
@@ -11,18 +11,48 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    exe.linkSystemLibrary("SDL2");
-    exe.linkFramework("CoreServices");
-    exe.linkFramework("CoreGraphics");
-    exe.linkFramework("Foundation");
+    if (target.result.os.tag == .windows) {
+        std.debug.print("Building for Windows\n", .{});
+        const sdl_path = std.process.getEnvVarOwned(b.allocator, "SDL2_PATH") catch {
+            std.debug.print("SDL2_PATH is missing in env\n", .{});
+            return;
+        };
+        defer b.allocator.free(sdl_path);
+        exe.linkLibC();
 
-    exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
-    exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
+        exe.addIncludePath(.{ .cwd_relative = b.fmt("{s}/include", .{sdl_path}) });
+        exe.addLibraryPath(.{ .cwd_relative = b.fmt("{s}/lib/x64", .{sdl_path}) });
+
+        exe.linkSystemLibrary("SDL2");
+        exe.linkSystemLibrary("SDL2main");
+        exe.linkSystemLibrary("shell32");
+
+        const dll_path = std.fs.path.join(
+            b.allocator,
+            &[_][]const u8{ sdl_path, "lib", "x64", "SDL2.dll" },
+        ) catch unreachable;
+        defer b.allocator.free(dll_path);
+
+        const install_step = b.addInstallFileWithDir(
+            .{ .cwd_relative = dll_path },
+            .bin,
+            "SDL2.dll",
+        );
+        b.getInstallStep().dependOn(&install_step.step);
+    } else {
+        std.debug.print("Building for MacOs\n", .{});
+        exe.linkSystemLibrary("SDL2");
+        exe.linkFramework("CoreServices");
+        exe.linkFramework("CoreGraphics");
+        exe.linkFramework("Foundation");
+        exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
+        exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
+    }
 
     b.installArtifact(exe);
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
@@ -37,7 +67,6 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 }
